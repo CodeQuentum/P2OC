2d58d306050786a4876c95455ec9db996b315ccc
Quentin Nicot <q.nicot@icloud.com>
2023-06-06 15:22:51 +0200

    probleme avec la route certainement

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,18 +1,21 @@
 const form = {
   email: document.querySelector('input[type="email"]'),
   password: document.querySelector('input[type="password"]'),
-  submit: document.querySelector('input[type="submit"]')
+  submit: document.querySelector('button')
 };
 console.log(form.email);
 console.log(form.password);
 console.log(form.submit);
-
+console.log(localStorage)
 let button = form.submit.addEventListener("click", (event)=>{
   event.preventDefault();
   const login = 'http://localhost:5678/api/users/login';
 
   fetch(login,{
     method: "POST",
+    mode: "cors",
+    cache:"no-cache",
+    credentials: "same-origin",
     headers:{
       Accept:"application/json, text/plain, */*",
       "Content-Type": "application/json",
