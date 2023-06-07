e738c7546dec4e468efaa87da3bc92e61fb1d72e
Quentin Nicot <q.nicot@icloud.com>
2023-06-07 13:26:21 +0200

    ajout local storage item

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,12 +6,12 @@ const form = {
 console.log(form.email);
 console.log(form.password);
 console.log(form.submit);
-console.log(localStorage)
+
 let button = form.submit.addEventListener("click", (event)=>{
   event.preventDefault();
-  const login = 'http://localhost:5678/api/users/login';
+  //const login = 'http://localhost:5678/api/users/login';
 
-  fetch(login,{
+  fetch('http://localhost:5678/api/users/login',{
     method: "POST",
     mode: "cors",
     cache:"no-cache",
@@ -26,7 +26,19 @@ let button = form.submit.addEventListener("click", (event)=>{
     }),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      if (data.error) {
+        console.error("error", data.message);
+        document.querySelector(".error-message-all")
+        .style.display = "block";
+        document.querySelector(".error-message-all")
+        .innerText =
+        "Mots de passe ou e-mail incorect, réésayez s'il vous plait";
+      } else {
+        localStorage.setItem("userId", JSON.stringify(data));
+        localStorage.setItem("token", 1)
+      }
+    })
     .catch((err) => {
       console.log(err);
     });
